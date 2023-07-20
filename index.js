6b38d9616c709f138d4b62fc74c1675688bee9dc
Samay Dhawale <samaydhawale1@gmail.com>
2023-07-20 15:37:54 +0530

    done with basic structures of backend app

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const app = express();
+const DBconnection = require("./config/db");
+
+app.use(express.json());
+
+
+app.get("/", async (req, res) => {
+  res.json({ msg: "welcome Music Mix backend" });
+});
+
+app.listen(8080, async () => {
+  await DBconnection
+  console.log("DB connected");
+  console.log("Server is running on port 8080");
+});
